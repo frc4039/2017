abbb4773b6910e60dfd0420f05c47a7e04534880
benjittk <torresb19>
2017-01-13 15:38:35 -0500

    STarted new 2017 robot project for testing a motor with gamepad button input

--- main/src/Robot.cpp
+++ main/src/Robot.cpp
@@ -1,49 +1,35 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include "WPILib.h"
 
 #include <IterativeRobot.h>
 #include <LiveWindow/LiveWindow.h>
-#include <SmartDashboard/SendableChooser.h>
-#include <SmartDashboard/SmartDashboard.h>
 
 class Robot: public frc::IterativeRobot {
 public:
+	Victor *m_leftDrive0; //4
+	Victor *m_leftDrive1; //1
+	Victor *m_rightDrive2; //2
+	Victor *m_rightDrive3; //3
+	VictorSP *m_thing;
+	Joystick *m_Joystick;
+	XboxController *m_Gamepad;
 	void RobotInit() {
-		chooser.AddDefault(autoNameDefault, autoNameDefault);
-		chooser.AddObject(autoNameCustom, autoNameCustom);
-		frc::SmartDashboard::PutData("Auto Modes", &chooser);
+		m_leftDrive0 = new Victor(0);
+		m_leftDrive1 = new Victor(1);
+		m_rightDrive2 = new Victor(2);
+		m_rightDrive3 = new Victor(3);
+		m_thing = new VictorSP(4);
+		m_Gamepad = new XboxController(0);
 	}
 
-	/*
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * GetString line to get the auto name from the text box below the Gyro.
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the
-	 * if-else structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
 	void AutonomousInit() override {
-		autoSelected = chooser.GetSelected();
-		// std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault);
-		std::cout << "Auto selected: " << autoSelected << std::endl;
 
-		if (autoSelected == autoNameCustom) {
-			// Custom Auto goes here
-		} else {
-			// Default Auto goes here
-		}
 	}
 
 	void AutonomousPeriodic() {
-		if (autoSelected == autoNameCustom) {
-			// Custom Auto goes here
-		} else {
-			// Default Auto goes here
-		}
+
 	}
 
 	void TeleopInit() {
@@ -51,19 +37,30 @@ public:
 	}
 
 	void TeleopPeriodic() {
-
+		operateThing();
 	}
 
 	void TestPeriodic() {
-		lw->Run();
+
 	}
 
-private:
-	frc::LiveWindow* lw = LiveWindow::GetInstance();
-	frc::SendableChooser<std::string> chooser;
-	const std::string autoNameDefault = "Default";
-	const std::string autoNameCustom = "My Auto";
-	std::string autoSelected;
+	void operateThing() {
+		if (m_Gamepad->GetYButton())
+			m_thing->SetSpeed(1.0);
+		else
+			m_thing->SetSpeed(0.0);
+	}
+
+	void teleDrive() {
+/*		leftSpeed = m_Joystick->GetY() - m_Joystick->GetX()
+		rightSpeed = 1) - scale(limit(expo(m_Gamepad2->GetRawAxis(4), 3), 1), 0.7f), PRACTICE_DRIVE_LIMIT) + scale(-limit(expo(m_Joystick->GetY(), 2), 1) - scale(limit(expo(m_Joystick->GetX(), 3), 1), 0.7f), PRACTICE_DRIVE_LIMIT) + scale(expo(-m_Gamepad->GetRawAxis(1), 2), 0.5) - scale(expo(m_Gamepad->GetRawAxis(0), 3), 0.5);
+
+		m_leftDrive0->SetSpeed(leftSpeed);
+		m_leftDrive1->SetSpeed(leftSpeed);
+		m_rightDrive2->SetSpeed(rightSpeed);
+		m_rightDrive3->SetSpeed(rightSpeed);*/
+	}
+
 };
 
 START_ROBOT_CLASS(Robot)
